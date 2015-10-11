05430f9a80934b25b4492fadb5f22cbb17748da2
Anton Sarov <izvanz3mno7o@yahoo.com>
2015-10-11 10:27:48 +0200

    login form submission

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import play.*;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
@@ -13,5 +15,12 @@ public class Application extends Controller {
 
     public Result login() {
         return ok(login.render("DevBay"));
+    }
+
+    public Result doLogin() {
+        DynamicForm dynamicForm = Form.form().bindFromRequest();
+        String username = dynamicForm.get("username");
+        String password = dynamicForm.get("password");
+        return TODO;
     }
 }
